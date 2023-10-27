dee412bef9515fa8de464c55f34822ff93234cd5
Matthew <49506518+Permafun@users.noreply.github.com>
2023-10-27 11:05:28 -0500

    Adjusting robot and sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,15 +1,18 @@
 import { allBlogs } from 'contentlayer/generated';
+import { MetadataRoute } from 'next';
 
-export default async function sitemap() {
+export default function sitemap(): MetadataRoute.Sitemap {
 	const blogs = allBlogs.map((post) => ({
 		url: `https://mattlittrell.dev/blog/${post.slug}`,
 		lastModified: post.publishedAt,
 	}));
 
-	const routes = ['', '/blog', '/projects', '/contact'].map((route) => ({
-		url: `https://mattlittrell.dev${route}`,
-		lastModified: new Date().toISOString().split('T')[0],
-	}));
+	const routes = ['', '/blog', '/extra-life', '/contact', '/feedback'].map(
+		(route) => ({
+			url: `https://mattlittrell.dev${route}`,
+			lastModified: new Date().toISOString().split('T')[0],
+		})
+	);
 
 	return [...routes, ...blogs];
 }
