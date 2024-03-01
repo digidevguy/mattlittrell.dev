914ca42f4b17d4f2d9ddca6ca78a2caea7773103
Matthew <49506518+Permafun@users.noreply.github.com>
2024-03-01 10:53:59 -0600

    Adjusting Typing

--- src/libs/sendgrid.ts
+++ src/libs/sendgrid.ts
@@ -1,12 +1,17 @@
 import sgMail from '@sendgrid/mail';
 
-export const sendEmail = async (subject, text) => {
+export const sendEmail = async (subject: string, text: string) => {
 	if (!process.env.SENDGRID_API_KEY) return;
 	sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+	if (!subject || !text) {
+		console.error('Subject and text are required');
+		return;
+	}
+
 	const msg = {
-		to: process.env.RECEIEVER_EMAIL,
-		from: process.env.SENDER_EMAIL,
+		to: process.env.RECEIEVER_EMAIL!,
+		from: process.env.SENDER_EMAIL!,
 		subject,
 		text,
 	};
